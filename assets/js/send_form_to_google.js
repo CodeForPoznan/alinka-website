855dbcea4f6dc7dedf266805ced24775d4a22e3b
Karol Jagodziński <karoljago@gmail.com>
2019-01-16 19:13:41 +0100

    Create a configfile to provide googleFormUrl. Improve send button

--- assets/js/send_form_to_google.js
+++ assets/js/send_form_to_google.js
@@ -1,7 +1,9 @@
+googleFormUrl = localGoogleFormUrl ? localGoogleFormUrl : googleFormUrl;
+
 function postContactToGoogle() {
     var email = $('#email').val();
     $.ajax({
-        url: "https://docs.google.com/forms/d/e/1FAIpQLSdBDdzs0F0aPlOgaUaF2c0qXhkQk4lCero7n0QaaUHUCxuVSQ/formResponse",
+        url: googleFormUrl,
         data: { "entry_771587508": email },
         type: "POST",
         dataType: "xml",
