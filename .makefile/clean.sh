3a40924313593dd15b079a25a4fbb28b5cecc523
Maciej Matuszczak <maciej.matuszczak96@gmail.com>
2019-05-08 18:15:16 +0200

    Revert "Setup environment, added HomePageView"

--- .makefile/clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-IMAGES=$(docker-compose images -q)
-docker-compose rm --force
-docker rmi $IMAGES
-
-docker network prune --force
-docker volume prune --force
--- .makefile/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-npm run build
-aws s3 sync build/ $DEPLOYMENT_BUCKET
-
