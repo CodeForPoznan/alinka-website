0ee3f8f32f90e675b5f11142ee3e94311fcb1fca
m-recz <m.reczuga@gmail.com>
2019-12-21 17:01:23 +0100

    Add in gulp task to create css sourcemap

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,19 @@
 
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var sourcemaps = require('gulp-sourcemaps');
 
 sass.compiler = require("node-sass");
 
-gulp.task("sass", function() {
+gulp.task("sass", function () {
   return gulp
     .src("assets/scss/*.scss")
+    .pipe(sourcemaps.init())
     .pipe(sass().on("error", sass.logError))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest("assets/css/"));
 });
 
-gulp.task("sass:watch", function() {
+gulp.task("sass:watch", function () {
   gulp.watch("assets/scss/*.scss", gulp.series("sass"));
 });
