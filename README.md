922fad7d0be09b292c9cd290c7bef1e1499a45b9
m-recz <m.reczuga@gmail.com>
2020-01-22 19:47:00 +0100

    Revert "Add changes requested in review".
    This reverts commit aeb953e51030a52c0164fcd3fba519be958d6f14.

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # How to start the project
-
 - (If you don't already have it, install node.js)
 - use `npm install` command to install the project
 - run `npm run sass` or `npx gulp sass` command to compile scss to css
-- run `npm run browser-sync` or `npx gulp browser-sync` command to start a development server
 - run `npm run watch` or `npx gulp sass:watch` command to watch changes in scss files
--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,20 @@
 "use strict";
 
 var gulp = require("gulp");
-var browserSync = require("browser-sync").create();
 var sass = require("gulp-sass");
-var sourcemaps = require("gulp-sourcemaps");
+var sourcemaps = require('gulp-sourcemaps');
 
 sass.compiler = require("node-sass");
 
-gulp.task("browser-sync", function() {
-  browserSync.init({
-    server: {
-      baseDir: "./"
-    }
-  });
-});
-
-gulp.task("sass", function() {
+gulp.task("sass", function () {
   return gulp
     .src("assets/scss/*.scss")
     .pipe(sourcemaps.init())
     .pipe(sass().on("error", sass.logError))
-    .pipe(sourcemaps.write("."))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest("assets/css/"));
 });
 
-gulp.task("sass:watch", function() {
+gulp.task("sass:watch", function () {
   gulp.watch("assets/scss/*.scss", gulp.series("sass"));
 });
