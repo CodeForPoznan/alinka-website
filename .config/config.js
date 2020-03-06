728ae1de92783b64e552322e75e6fc5844362d06
Tomasz Magulski <tomasz@magul.ski>
2020-03-07 00:02:37 +0100

    Google Forms url hidden behind AWS CloudFront (#43)

--- .config/config.js
+++ .config/config.js
@@ -1,2 +1,2 @@
-var googleFormUrl = "https://docs.google.com/forms/u/3/d/e/1FAIpQLSd9dThRCpWYRo-b0KMzjFw3aNa9CTXvmcpPzHqJwIFRxLQ1QQ/formResponse";
+var googleFormUrl = "https://alinka.io/forms/u/3/d/e/1FAIpQLSd9dThRCpWYRo-b0KMzjFw3aNa9CTXvmcpPzHqJwIFRxLQ1QQ/formResponse";
 var googleFormEntry = "entry_1890980143";
